7582fb2bda72f727a2c57a7cc9d0fd0fbbd9ab49
Appaso Borate <appaso1686@gmail.com>
2023-01-13 11:54:52 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Static-Sean-With-TextBox.github.io" 
---
+++ __settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1636731.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+SCRIPTS = [ 117844853, 117844854, 117844856, 117844858 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
